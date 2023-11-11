55acb1d664cdcd529a1a491932266b513fb4eb89
alfonsoalarcon <alfonsoalarcon@msn.com>
2023-11-11 06:40:29 +0100

    Modifications in the figure

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ df = pd.read_csv(file_path, sep=",", skipinitialspace=True)
 df1 = df[["Country", "Life expectancy"]]
 df1["Country"] = df1["Country"].astype("category")
 df1["Life expectancy"] = df1["Life expectancy"].astype("float")
-mean_value = df1["Life expectancy"].mean()
-print("Global life expectancy mean value is {}".format(mean_value))
 df1.sort_values(by="Life expectancy", ascending=False, inplace=True)
 
 df2 = df1.iloc[:10]
@@ -18,7 +16,7 @@ print(df2.head(10))
 plt.figure(figsize=(10,10))
 
 bars = plt.barh(df2["Country"], df2["Life expectancy"], color='lightgray')
-plt.title('The 10 countries with more life expectancy', fontsize=20)
+plt.title('The 10 countries with the highest life expectancy', fontsize=20)
 plt.xlim(65, 88)
 plt.xlabel('Life expectancy (Years)', fontsize=20)
 plt.ylabel('Countries', fontsize=20)
